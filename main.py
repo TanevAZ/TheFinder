f4580ea6ae8415a827f85477fe9957e191d1da40
Tanev <66444552+TanevAZ@users.noreply.github.com>
2023-07-17 18:37:01 +0200

    main file

--- main.py
+++ main.py
@@ -5,10 +5,9 @@ from colorama import Fore
 pm = platform.system()
 if pm == "Windows":
     os.system("cls")
-elif pm == "Linux":
+elif pm == "Linux" or pm == "Darwin":
     os.system("clear")
-elif pm == "Darwin":
-    os.system("clear")
+
 banner = Fore.RED + f"""
                          _____ _         ______ _           _           
                         |_   _| |        |  ___(_)         | |          
@@ -23,20 +22,29 @@ banner = Fore.RED + f"""
 print(banner)
 
 def main():
-    # ask user for directory to search
+    # Ask the user for the directory to search
     directory = input(f'\n{Fore.RESET}Enter the directory to search : ')
-    # ask user for file name to search for
+    # Ask the user for the file name to search for
     file = input(f'Enter the file name : {Fore.LIGHTGREEN_EX}')
-    # search for file in directory and all subdirectories
-    found = False
+    # Search for the file in the directory and all subdirectories
+    pathFound = []
     for root, dirs, files in os.walk(directory):
         for name in files:
             if name == file:
-                print(f'{Fore.RESET}File {Fore.LIGHTGREEN_EX}found(s){Fore.RESET} in : {root}\\{name}')
-                found = True
-    if not found:
+                path = os.path.join(root, name)
+                print(f'{Fore.RESET}File {Fore.LIGHTGREEN_EX}found{Fore.RESET} in : {path}')
+                pathFound.append(path)
+
+    if pathFound:
+        with open("results.txt", "w") as file:
+            for path in pathFound:
+                file.write(path + "\n")
+        resultFile = os.path.join(os.getcwd(), "results.txt")
+        print(f"\n{Fore.RESET}File results.txt saved in : {Fore.LIGHTYELLOW_EX}{resultFile}")
+    else:
         print(f'{Fore.RESET}File not {Fore.LIGHTRED_EX}found{Fore.RESET}')
-    input(f"\n{Fore.LIGHTGREEN_EX}Scan Terminated. {Fore.WHITE}Press Enter to Exit...")
+
+    input(f"\n{Fore.LIGHTGREEN_EX}Scan terminated. \n{Fore.WHITE}Press Enter to exit...")
     exit()
 
 main()
